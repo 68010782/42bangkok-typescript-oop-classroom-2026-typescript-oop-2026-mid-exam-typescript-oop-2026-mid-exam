6c25d37868d993ca9ac1b9e7c1e718710a197128
68010782 <68010782@kmitl.ac.th>
2026-01-21 14:56:12 +0700

    p01,02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,17 @@
 // Write your code below
+
+const score = +process.argv[2];
+
+if (Number.isNaN(score) || score < 0 || score > 100){
+    console.log("Invalid input");
+}else if (score >= 80){
+    console.log("Grade is A");
+}else if (score >= 70){
+    console.log("Grade is B");
+}else if (score >= 60){
+    console.log("Grade is C");
+}else if (score >= 50){
+    console.log("Grade is D");
+}else {
+    console.log("Grade is F");
+}
